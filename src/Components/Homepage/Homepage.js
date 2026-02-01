296b1bcfd0afe06cca228b91287721bc798e9dcd
codewingx-13 <codewingx@gmail.com>
2026-02-02 00:47:18 +0530

    done20

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -5,11 +5,11 @@ import Services from '../Services/Services';
 import Footer from '../Footer/Footer';// Updated import
 import Hero from '../Hero/Hero';
 import Solutions from '../Services/Solutions';
-import Stories from '../Stories/Stories';
-import WhyChoose from '../Stories/WhyChoose';
-import OurPatner from '../About/Approach/OurPartners/OurPartners';
+// import Stories from '../Stories/Stories';
+// import WhyChoose from '../Stories/WhyChoose';
+// import OurPatner from '../About/Approach/OurPartners/OurPartners';
 import OurTeam from '../About/Company/OurTeam/OurTeam';
-import Founder from '../Founder/Founder';
+// import Founder from '../Founder/Founder';
 const Homepage = () => {
   return (
     <div>
